a39ae6095b14d27f56400959a302a3476362fa0a
Elliot Courant <me@elliotcourant.dev>
2026-05-02 16:28:34 -0500

    chore: Upgrading to typescript v6

--- src/helpers.ts
+++ src/helpers.ts
@@ -119,7 +119,6 @@ export function chain<T>(...fns: T[]) {
   return (...args: T extends AnyFunction ? Parameters<T> : never) => {
     for (const fn of fns) {
       if (typeof fn === 'function') {
-        // @ts-expect-error
         fn(...args);
       }
     }
